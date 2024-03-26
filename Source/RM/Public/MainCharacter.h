920df482963e6589b3b56bdffff215359f98cda5
Glerik <glerik1337@gmail.com>
2024-03-26 17:43:21 +0200

    Added stamina for the character, its restoration and decrease, sound when stamina ends

--- Source/RM/Public/MainCharacter.h
+++ Source/RM/Public/MainCharacter.h
@@ -1,13 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
+#pragma once // Пiдключення бiблiотеки та файлiв 1 раз.
 
-#include "CoreMinimal.h"
-#include "GameFramework/Character.h"
-#include "MainCharacter.generated.h"
+#include "CoreMinimal.h" // Ядро UE.
+#include "GameFramework/Character.h" // Пiдключення class Character.
+#include "MainCharacter.generated.h" // Генератор коду для UE (обов'язково остання в пiдключеннi).
 
-UCLASS()
-class RM_API AMainCharacter : public ACharacter
+UCLASS() // Макрос що пояснює UE, що нижче йде class.
+class RM_API AMainCharacter : public ACharacter // Пiдключення API для клонування class ACharacter для нового.
 {
 	GENERATED_BODY()
 
@@ -15,4 +15,14 @@ public:
 	// Sets default values for this character's properties
 	AMainCharacter();
 
+	float CharacterStamina; // Витривалість персонажу.
+	float GetStaminaPercent(); // Функцiя, яка буде повертати значення float.
+	float AddStamina(); // Функцiя, яка буде повертати витривалiсть.
+	float DeacreaseStamina(); // Функцiя, яка буде вiднiмати витривалiсть.
+
+	bool StaminaZero(); // Функція, яка не дозволяє бігати за відсутності витривалості.
+
+	USoundBase* TiredSound; // Змiнна, для вiдтворення звуку, за вiдсутностi витривалостi (Покажчик * - якщо об'єкт iснує: виконувати код).
+
+	
 };
