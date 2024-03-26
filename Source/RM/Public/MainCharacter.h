18aaadc0300d2e553fec6ef504885c49ca0dd9ce
Glerik <glerik1337@gmail.com>
2024-03-26 20:06:55 +0200

    Added bIsTired variable, macro for displaying in BP

--- Source/RM/Public/MainCharacter.h
+++ Source/RM/Public/MainCharacter.h
@@ -15,13 +15,18 @@ public:
 	// Sets default values for this character's properties
 	AMainCharacter();
 
+	UPROPERTY(EditDefaultsOnly, Category = "CharactarStamina") // Макрос, для вiдображення CharacterStamina в BP.
 	float CharacterStamina; // Витривалість персонажу.
+
 	float GetStaminaPercent(); // Функцiя, яка буде повертати значення float.
 	float AddStamina(); // Функцiя, яка буде повертати витривалiсть.
 	float DeacreaseStamina(); // Функцiя, яка буде вiднiмати витривалiсть.
 
-	bool StaminaZero(); // Функція, яка не дозволяє бігати при відсутності витривалості.
+	bool StaminaZero(); // Функція, перевiряє, чи дорiвнює витривалiсть нулю.
 
+	bool bIsTired; // Функція, яка не дозволяє бігати при відсутності витривалості.
+
+	UPROPERTY(EditDefaultsOnly, Category = "CharactarStamina") // Макрос, для TiredSound CharacterStamina в BP.
 	USoundBase* TiredSound; // Змiнна, для вiдтворення звуку, при вiдсутностi витривалостi (Покажчик * - якщо об'єкт iснує: виконувати код).
 
 	
