5da162cdb86cd5375fb97f5313e236aee139cbb2
Glerik <glerik1337@gmail.com>
2024-03-26 16:58:24 +0200

    changing the location of game objects

---
+++ Source/RM/RM.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RM, "RM" );
