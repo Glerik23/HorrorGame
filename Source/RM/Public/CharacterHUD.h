dee3602099059300f080180121b94606274d75b6
Glerik <glerik1337@gmail.com>
2024-03-31 18:10:07 +0300

    Added start for Stamina output in HUD

---
+++ Source/RM/Public/CharacterHUD.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CharacterHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RM_API ACharacterHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+
+	/* / Script / UMGEditor.WidgetBlueprint'/Game/Blueprints/WBP_Stamina.WBP_Stamina*/
+};
