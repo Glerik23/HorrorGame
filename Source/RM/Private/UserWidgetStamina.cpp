0755563d69b4a3599726581830f53b1fec60d67f
Glerik <glerik1337@gmail.com>
2024-04-21 17:44:46 +0300

    create blueprint for door

--- Source/RM/Private/UserWidgetStamina.cpp
+++ Source/RM/Private/UserWidgetStamina.cpp
@@ -8,7 +8,7 @@
 float UUserWidgetStamina::GetCharacterStamina()
 {
 	APlayerCharacter* OurPlayer = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)); // Перевірка, чи PlayerCharacter - Character, і де він знаходиться(GetWorld), та знаходиться під індексом 0.
-	if (OurPlayer != nullptr) {
+	if (OurPlayer != nullptr) { // Перевiрка що character iснуэ.
 
 		float GetStamina = OurPlayer->Stamina / 100.f; // GetStamina = OurPlayer->Stamina(витривалiсть) / (подiлена) на 100. (операцiя потрiбна, для того щоб вывести значення в HUD (в значеннi HUD э 1, витривалiсть персонажу э 100, щоб вивести значення, треба щоб воно, було не бiльше 1).)
 		return GetStamina; // Повернення GetStamina.
