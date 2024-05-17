c5560e65b79830f9f48fa12f18fcd72b62feb5fe
Glerik <glerik1337@gmail.com>
2024-05-17 21:28:07 +0300

    Changing the location of BP files (sorting)

--- Source/RM/Private/PlayerGameMode.cpp
+++ Source/RM/Private/PlayerGameMode.cpp
@@ -5,7 +5,7 @@
 
 APlayerGameMode::APlayerGameMode() : Super() {
 
-	ConstructorHelpers::FClassFinder<APawn> MainCharacterPlayerCharacter(TEXT("/Game/Blueprints/BP_Player")); // Назначення iгровим персонажем BP_Player.
+	ConstructorHelpers::FClassFinder<APawn> MainCharacterPlayerCharacter(TEXT("/Game/Blueprints/Player/BP_Player")); // Назначення iгровим персонажем BP_Player.
 
 	DefaultPawnClass = MainCharacterPlayerCharacter.Class; // За замовчуванням BP_Player основний персонаж.
 	HUDClass = ACharacterHUD::StaticClass(); // HUD за замовчуванням.
