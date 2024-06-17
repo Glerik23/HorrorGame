7d92013eeaae161dc8499c572f686bb0ad446e1f
Glerik <glerik1337@gmail.com>
2024-06-17 14:08:10 +0300

    TestItem cook error fix

--- Source/RM/Private/StreetDoor.cpp
+++ Source/RM/Private/StreetDoor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Inventory.h"
 #include "StreetDoor.h"
+#include "Inventory.h"
 #include "PlayerCharacter.h"
 #include "Components/StaticMeshComponent.h"
 
